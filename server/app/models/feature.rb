91d3b1d3dd910a00c88d09c93acb84db541b8dab
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-04-12 23:08:43 -0300

    se definen las validaciones para el modelo de la base de datos

--- server/app/models/feature.rb
+++ server/app/models/feature.rb
@@ -1,2 +1,8 @@
 class Feature < ApplicationRecord
+
+    validates :external_id, :place, :external_url, :magType, :title, presence: true
+    validates :magnitude, numericality: { greater_than_or_equal_to: -1.0, less_than_or_equal_to: 10.0 }
+    validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90.0, less_than_or_equal_to: 90.0 }
+    validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180.0, less_than_or_equal_to: 180.0 }
+
 end
