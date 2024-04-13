f108a59cfffb1bba37455f1562d897fad2168d4d
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-04-12 23:14:29 -0300

    se realiza la migracion a la base de datos

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,8 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_13_015937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "features", force: :cascade do |t|
+    t.string "external_id"
+    t.decimal "magnitude"
+    t.string "place"
+    t.string "time"
+    t.string "external_url"
+    t.boolean "tsunami"
+    t.string "magType"
+    t.string "title"
+    t.decimal "latitude"
+    t.decimal "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
