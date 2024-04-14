d5fe6a26a87039146991068a6fc894b558267c3f
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-04-14 01:32:56 -0300

    se corrige el formato de datos que devuelve la api en su ruta get

--- server/app/controllers/api/v1/features_controller.rb
+++ server/app/controllers/api/v1/features_controller.rb
@@ -9,10 +9,24 @@ class Api::V1::FeaturesController < ApplicationController
   features = features.page(params[:page]).per(params[:per_page])
 
   serialized_features = ActiveModel::Serializer::CollectionSerializer.new(
-    features,
-    serializer: FeatureSerializer
-  )
+      features,
+      serializer: FeatureSerializer
+    )
 
-    render json: serialized_features, status: :ok
+  render json: {
+    data: serialized_features,
+    pagination: pagination_data(features)
+  }, status: :ok
+  end
+
+  private
+
+  def pagination_data(collection)
+    total_pages = (collection.total_count.to_f / collection.limit_value).ceil
+    {
+      current_page: collection.current_page,
+      total: total_pages,
+      per_page: collection.limit_value
+    }
   end
 end
